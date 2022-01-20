991222bb60cbea38697ab8777a07391c0b9c63ea
Tim Salzmann <Tim.Salzmann@web.de>
2022-01-20 15:48:52 +0100

    Added catkin package files

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+from catkin_pkg.python_setup import generate_distutils_setup
+
+setup_args = generate_distutils_setup(
+    packages=['ml_casadi'],
+    package_dir={'': '.'}
+)
+
+setup(**setup_args)
